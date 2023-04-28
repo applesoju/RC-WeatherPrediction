38880acf110140ab0409d463fd2c7cdccfc11d96
AppleSoju <patryknmarciniak@gmail.com>
2023-04-28 09:44:31 +0200

    implemented train() method and other utility methods used for plotting

--- resources/reservoir_computing.py
+++ resources/reservoir_computing.py
@@ -16,7 +16,10 @@ class SimpleESN:
         self.reservoir = None
         self.spectral_radius = None
 
-    def loadtxt(self, filepath, missing_values=None):
+        self.output_weigths = None
+        self.x_out = None
+
+    def loadtxt(self, filepath, missing_values=None, save_to_file=None):
         try:
             self.data = np.loadtxt(filepath)
         except (ValueError, FileNotFoundError) as err:
@@ -42,6 +45,15 @@ class SimpleESN:
                     new_values = np.linspace(self.data[i - 1], self.data[next_ind], next_ind - i + 2)
                     self.data[i: next_ind] = new_values[1: -1]
 
+        if save_to_file is not None:
+            try:
+                with open(save_to_file, "w") as f:
+                    f.write("\n".join([str(i) for i in self.data]))
+            except OSError as err:
+                print(f"Error: {err}\n"
+                      f"The target directory does not exist.\n"
+                      f"Skipping saving to file.\n")
+
     def plot_data(self, labels=None, length=None):
         if self.data is None:
             raise TypeError("Error: data has not been loaded.")
@@ -49,7 +61,7 @@ class SimpleESN:
         if length is None:
             length = len(self.data)
 
-        plt.figure(figsize=(20, 12)).clear()
+        plt.figure(1, figsize=(20, 12)).clear()
         plt.plot(self.data[:length])
         plt.title("Sample of loaded data", fontsize=20)
 
@@ -77,7 +89,102 @@ class SimpleESN:
         rho = max(abs(np.linalg.eig(self.reservoir)[0]))
         self.reservoir *= self.spectral_radius / rho
 
-        print("ok")
+    def train(self, initial_length, training_length):
+        x_out = np.zeros((1 + self.input_size + self.reservoir_size, training_length - initial_length))
+        y_target = self.data[None, initial_length + 1: training_length + 1]
+        x = np.zeros((self.reservoir_size, 1))
 
-    def train(self):
-        raise NotImplementedError
+        for t in range(initial_length, training_length):
+            u = self.data[t]
+            x = (1 - self.leaking_rate) * x + self.leaking_rate * \
+                np.tanh(np.dot(self.input_weights, np.vstack((1, u))) +
+                        np.dot(self.reservoir, x))
+
+            x_out[:, t - initial_length] = np.vstack((1, u, x))[:, 0]
+
+        reg_coeff = 1e-8
+        self.output_weigths = np.linalg.solve(
+            np.dot(x_out, x_out.T) + reg_coeff * np.eye(1 + self.input_size + self.reservoir_size),
+            np.dot(x_out, y_target.T)
+        ).T
+
+        self.x_out = x_out
+
+        return x
+
+    def predict(self, test_length, training_length, initial_length, last_x, mode, save_to_file=None):
+        y_test = np.zeros((self.output_size, test_length))
+        u = self.data[training_length + initial_length]
+        x = last_x
+        u1 = u2 = u
+
+        for t in range(test_length):
+            x = (1 - self.leaking_rate) * x + self.leaking_rate * \
+                np.tanh(np.dot(self.input_weights, np.vstack((1, u))) +
+                        np.dot(self.reservoir, x))
+            y = np.dot(self.output_weigths, np.vstack((1, u, x)))
+            y_test[:, t] = y
+
+            match mode:
+                case "g":
+                    u = y
+                case "p":
+                    u = self.data[training_length + initial_length + t + 1]
+                case _:
+                    raise ValueError("Error: parameter 'mode' has to be either 'g' for 'generative'"
+                                     "or 'p' for 'predictive.")
+
+        if save_to_file is not None:
+            try:
+                with open(save_to_file, "w") as f:
+                    f.write("\n".join([str(i) for i in y_test[0]]))
+            except OSError as err:
+                print(f"Error: {err}\n"
+                      f"The target directory does not exist.\n"
+                      f"Skipping saving to file.\n")
+
+        return y_test
+
+    def plot_reservoir_activations(self, xlen, ylen):
+        plt.figure(2, figsize=(20, 12)).clear()
+        plt.plot(self.x_out[0: xlen, 0: ylen])
+        plt.title("Some reservoir activations")
+
+        plt.show()
+
+    def plot_output_weigths(self):
+        plt.figure(3, figsize=(20, 12)).clear()
+        plt.bar(np.arange(1 + self.input_size + self.reservoir_size), self.output_weigths[0].T)
+        plt.title("Output weights")
+
+        plt.show()
+
+    def plot_prediction_with_error(self, prediction, initial_length, training_length, test_length):
+
+        err = np.square(self.data[training_length + initial_length + 1:
+                                  training_length + initial_length + test_length + 1] - prediction[0])
+        mse = sum(err) / test_length
+
+        print(f"MSE: {mse}")
+
+        plt.figure(4, figsize=(20, 12)).clear()
+        plt.plot(self.data[training_length + initial_length + 1:
+                           training_length + initial_length + test_length + 1], "g")
+        plt.plot(prediction.T, "b")
+        plt.title("Target and generated signals")
+        plt.legend(["Target signal", "Predicted signal"])
+
+        plt.figure(5, figsize=(20, 12)).clear()
+
+        plt.subplot(211)
+        plt.plot(err)
+        plt.yscale("log")
+
+        plt.subplot(212)
+        plt.plot(err)
+        plt.yscale("linear")
+
+        plt.title(r"MSE")
+
+        plt.show()
+
