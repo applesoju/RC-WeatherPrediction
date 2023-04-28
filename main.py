b08b13cc09f064bda14ee7b743bc235039c472c8
AppleSoju <patryknmarciniak@gmail.com>
2023-04-28 12:41:10 +0200

    adjusted main.py to changes made

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
-from resources.reservoir_computing import simple_esn
-from resources.weather_data_processing import weather_csv_to_timeseries, convert_columns_to_float, rescale_columns
+from resources.tmp_timeseries import TmpTimeseries
+from resources.reservoir_computing import SimpleESN
+
+TRAINING_LEN = 5000
+TEST_LENGTH = 1000
 
 
 def main():
-    station, df = weather_csv_to_timeseries(filepath="testing/07003099999.csv",
-                                            attribute_name="TMP")
-    df = convert_columns_to_float(df_in=df,
-                                  column_names="TMP")
-    df = rescale_columns(df_in=df,
-                         column_names="TMP",
-                         scale_by=0.1)
+    # tmp_ts = TmpTimeseries()
+    # tmp_data = tmp_ts.load_csv(filepath="testing/07003099999.csv",
+    #                            column_names=["DATE", "TMP"])
+    #
+    # tmp_dataframe = tmp_ts.get_tmp_dataframe(tmp_data)
+    # tmp_ts.save_csv("testing/out_test.csv")
+    # tmp_ts.save_txt("testing/timeseries.txt")
+    #
+    # tmp_timeseries = tmp_ts.get_tmp_timeseries(tmp_data)
+
+    simple_esn = SimpleESN()
+
+    # Load the data
+    simple_esn.loadtxt(filepath="testing/timeseries.txt")
+
+    # # Plot loaded data
+    simple_esn.plot_data(labels=["day", "temperature [\u2103]"],
+                         length=2000)
+
+    # Initialize the reservoir
+    simple_esn.initialize_reservoir(input_size=1,
+                                    reservoir_size=1000,
+                                    leaking_rate=0.25,
+                                    spectral_radius=1.25,
+                                    seed=64)
+
+    # Perform training
+    res = simple_esn.train(training_length=TRAINING_LEN)
+
+    # Plot some activations of the reservoir
+    # simple_esn.plot_reservoir_activations(10, 10)
+
+    # Plot trained output weigths
+    # simple_esn.plot_output_weigths()
+
+    # Predict data
+    pred = simple_esn.predict(test_length=TEST_LENGTH,
+                              training_length=TRAINING_LEN,
+                              last_x=res,
+                              mode="p",
+                              save_to_file="testing/prediction.txt")
+
+    # Plot predicted data with target data and error
+    simple_esn.plot_prediction_with_error(prediction=pred,
+                                          training_length=TRAINING_LEN,
+                                          test_length=TEST_LENGTH)
 
     print("DONE")
 
 
 if __name__ == "__main__":
-    # main()
-    simple_esn(timeseries_filepath="MackeyGlass_t17.txt",
-               training_length=0,
-               test_length=0)
+    main()
