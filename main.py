be087a66156850044b8c539580afea49b9552d17
AppleSoju <patryknmarciniak@gmail.com>
2023-05-07 15:55:57 +0200

    adjusted main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from resources.tmp_timeseries import TmpTimeseries
 TRAINING_LEN = 5000
 TEST_LENGTH = 2000
 MODEL_PARAMS = [
-    1,  # Input size
     1000,  # Reservoir size
     0.25,  # Leaking rate
     1.25,  # Spectral radius
@@ -14,56 +13,61 @@ MODEL_PARAMS = [
 
 
 def main():
-    tmp_ts = TmpTimeseries()
-    # tmp_da    ta = tmp_ts.load_csv(filepath="testing/07003099999.csv",
+    # tmp_ts = TmpTimeseries()
+    # tmp_data = tmp_ts.load_csv(filepath="data/3289811.csv",
     #                            column_names=["DATE", "TMP"])
 
-    # tmp_dataframe = tmp_ts.get_tmp_dataframe(tmp_data)
-    # tmp_ts.save_csv("testing/out_test.csv")
-    # tmp_ts.save_txt("testing/timeseries.txt")
-    #
+    # tmp_dataframe = tmp_ts.get_tmp_dataframe(tmp_df=tmp_data,
+    #                                          restrain_year_range=(2000, 2022))
+    # tmp_ts.save_csv("data/2000-2022/data.csv")
+    # tmp_ts.save_txt("data/2000-2022/timeseries.txt")
+
     # tmp_timeseries = tmp_ts.get_tmp_timeseries(tmp_data)
 
     simple_esn = SimpleESN()
 
     # Load the data
-    simple_esn.loadtxt(filepath="testing/timeseries.txt")
+    simple_esn.loadtxt(filepath="data/2000-2022/timeseries.txt")
 
     # # Plot loaded data
-    # simple_esn.plot_data(labels=["day", "temperature [\u2103]"],
-    #                      length=2000)
+    simple_esn.plot_data(labels=["day", "temperature [C]"],
+                         length=2000)
 
     # Initialize the reservoir
-    # simple_esn.initialize_reservoir(input_size=1,
-    #                                 reservoir_size=1000,
-    #                                 leaking_rate=0.25,
-    #                                 spectral_radius=1.25,
-    #                                 seed=64)
+    simple_esn.initialize_reservoir(*MODEL_PARAMS)
+
+    # Load model
+    # simple_esn.load_reservoir_from_file("data/2000-2022/models/model_009.json")
 
     # # Perform training
-    # res = simple_esn.train(training_length=TRAINING_LEN)
-    #
-    # # Plot some activations of the reservoir
-    # # simple_esn.plot_reservoir_activations(10, 10)
-    #
-    # # Plot trained output weigths
-    # # simple_esn.plot_output_weigths()
-    #
-    # # Predict data
-    # pred = simple_esn.predict(test_length=TEST_LENGTH,
-    #                           training_length=TRAINING_LEN,
-    #                           last_x=res,
-    #                           save_to_file="testing/prediction.txt")
-    # #
-    # # # Plot predicted data with target data and error
-    # simple_esn.plot_prediction_with_error(prediction=pred,
-    #                                       training_length=TRAINING_LEN,
-    #                                       test_length=TEST_LENGTH)
+    simple_esn.train(training_length=TRAINING_LEN)
 
-    modeval = ModelEvaluation(model=simple_esn,
-                              model_params=MODEL_PARAMS)
+    # Plot some activations of the reservoir
+    simple_esn.plot_reservoir_activations(10, 10)
 
-    modeval.cross_validate(10)
+    # Plot trained output weigths
+    simple_esn.plot_output_weights()
+
+    # Predict data
+    pred = simple_esn.predict(test_length=TEST_LENGTH,
+                              training_length=TRAINING_LEN,
+                              save_to_file="testing/prediction.txt")
+
+    # Plot predicted data with target data and error
+    simple_esn.plot_prediction_with_error(prediction=pred,
+                                          training_length=TRAINING_LEN,
+                                          test_length=TEST_LENGTH)
+
+    # modeval = ModelEvaluation(model=simple_esn,
+    #                           model_params=MODEL_PARAMS)
+
+    # modeval.cross_validate(n_of_splits=10,
+    #                        save_results_to_file="data/2000-2022/errors.csv",
+    #                        save_models_to_dir="data/2000-2022/models",
+    #                        save_models="all")
+    #
+    # modeval.plot_metrics()
+
 
     print("DONE")
 
