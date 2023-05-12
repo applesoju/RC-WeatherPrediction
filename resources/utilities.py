9dadb4124ad333c6f32b950eab02b353779fa58d
AppleSoju <patryknmarciniak@gmail.com>
2023-05-12 14:02:51 +0200

    implemented dataframe sorting using specified split or mean metric value

--- resources/utilities.py
+++ resources/utilities.py
@@ -21,14 +21,31 @@ def get_metric_from_gs(path, metric):
         errors_df = pd.read_csv(fullpath)
         metric_list.append(errors_df[metric].tolist())
 
-    metric_df = pd.DataFrame(metric_list, index=models, columns=[str(i) for i in range(len(metric_list[0]))])
+    metric_df = pd.DataFrame(metric_list, index=models, columns=[str(i + 1) for i in range(len(metric_list[0]))])
 
     return metric_df
 
 
-def sort_df_by_split(df, split, desc=True):
-    raise NotImplementedError
+def sort_df_by_split(df, split, desc=True, get_best=None):
+    sorted_df = df.sort_values(by=[*split], ascending=not desc)
+
+    if get_best is not None:
+        sorted_df = sorted_df.head(get_best)
+
+    return sorted_df
 
 
-def sortdf_by_mean(df):
-    raise NotImplementedError
+def sort_df_by_mean(df, desc=True, get_best=None):
+    row_means = df.mean(axis=1)
+
+    if desc:
+        reverse_idx = row_means.argsort()[::-1]
+        sorted_df = df.iloc[reverse_idx]
+
+    else:
+        sorted_df = df.iloc[row_means.argsort()]
+
+    if get_best is not None:
+        sorted_df = sorted_df.head(get_best)
+
+    return sorted_df
